844b2aa905654e2dc82456c347670570a861df86
wortit988 <106458453+wortit988@users.noreply.github.com>
2023-10-06 00:13:32 +0530

    Setting up tailwind, react-route-dom and basic structure

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/893a42ad-6a39-43c2-bbc1-a951ec64ed6d/1d86e0ac-428c-4dfa-9810-5251dbf446f8/IN-en-20231002-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="background"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Login
